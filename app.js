94beb49ab699d38467a2a63a53065fd504ef9f01
T Fleck <emailtfleck@gmail.com>
2019-09-22 17:57:24 -0400

    formatting consistency

--- app.js
+++ app.js
@@ -1,107 +1,124 @@
-const express = require('express'),
-      fs = require('fs')
-const pkg = require( './package.json' )
-const app = express()
-const protocol = 'https'
-const port = 3000
-const host = 'localhost'
-const key_path = './certs/encrypted.key'
-const cert_path = './certs/server.crt'
+const express = require("express"),
+  fs = require("fs"),
+  pkg = require("./package.json");
 
-//Load list of Scrabble words
-if (fs.existsSync('scrabble_words.txt')) {
-  var wordList = fs.readFileSync('scrabble_words.txt').toString().split("\n")
+const app = express();
+const protocol = "https";
+const port = 3000;
+const host = "localhost";
+const key_path = "./certs/encrypted.key";
+const cert_path = "./certs/server.crt";
+
+// Load list of Scrabble words
+if (fs.existsSync("scrabble_words.txt")) {
+  var wordList = fs
+    .readFileSync("scrabble_words.txt")
+    .toString()
+    .split("\n");
   wordList = wordList.map(s => s.trim());
-  //Sort word list by length & alphabetically
+  // Sort word list by length & alphabetically
   wordList.sort((a, b) => a.length - b.length || a.localeCompare(b));
-}
-else{
-  console.error("Could not find list of scrabble words")
-  process.exit(1)
-}
-
-//Load list of Scrabble definitions
-if (fs.existsSync('scrabble_definitions.txt')) {
-  var wordDefs = fs.readFileSync('scrabble_definitions.txt').toString().split("\n")
-  //Convert definitions list into key-value pairs
-  wordDefs = wordDefs.map(s => s.trim().split("\t"))
-  wordDefs = objectify(wordDefs)
-}
-else{
-  console.error("Could not find list of scrabble definitions")
-  process.exit(1)
+} else {
+  console.error("Could not find list of scrabble words");
+  process.exit(1);
 }
 
-console.log("Read "+String(wordList.length)+" words & definitions from dictionary")
+// Load list of Scrabble definitions
+if (fs.existsSync("scrabble_definitions.txt")) {
+  var wordDefs = fs
+    .readFileSync("scrabble_definitions.txt")
+    .toString()
+    .split("\n");
+  // Convert definitions list into key-value pairs
+  wordDefs = wordDefs.map(s => s.trim().split("\t"));
+  wordDefs = objectify(wordDefs);
+} else {
+  console.error("Could not find list of scrabble definitions");
+  process.exit(1);
+}
 
-app.get('/', function(request, response) {
-    response.sendFile(__dirname + '/public_html/index.html')
-})
+console.log(
+  "Read " + String(wordList.length) + " words & definitions from dictionary"
+);
 
-app.get('/style.css', function(request, response) {
-    response.sendFile(__dirname + '/public_html/style.css')
-})
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + "/public_html/index.html");
+});
 
-app.get('/main.js', function(request, response) {
-    response.sendFile(__dirname + '/public_html/main.js')
-})
+app.get("/style.css", function(request, response) {
+  response.sendFile(__dirname + "/public_html/style.css");
+});
 
-app.get('/api/randomword', function(request, response) {
-  let rand = Math.floor(Math.random()*wordList.length)
-  while(wordList[rand].length < 5 || wordList[rand].length > 9){
-    rand = Math.floor(Math.random()*wordList.length)
+app.get("/main.js", function(request, response) {
+  response.sendFile(__dirname + "/public_html/main.js");
+});
+
+// Get a random word from the dictionary that's not too hard or too easy
+app.get("/api/randomword", function(request, response) {
+  let rand = Math.floor(Math.random() * wordList.length);
+  while (wordList[rand].length < 5 || wordList[rand].length > 9) {
+    rand = Math.floor(Math.random() * wordList.length);
   }
-  response.send(wordList[rand].trim())
-})
+  response.send(wordList[rand].trim());
+});
 
-app.get('/api/getwords', function(request,response) {
-  let word = request.query.word.trim().toUpperCase()
-  let new_subwords = [word]
-  let word_alpha = word.split('').sort()
-  for( let i=0; i < wordList.length; i++  ){
-    let word_temp = [...word_alpha]
-    let key = wordList[i].trim()
-    if(key.length > 2 && key.length <= word_alpha.length){
-      let key_alpha = key.split('').sort()
-      let j=0
-      for( j=0; j < key_alpha.length; j++ ){
-        let ind = word_temp.indexOf(key_alpha[j])
-        if( ind >= 0 ){
-          word_temp.splice(ind,1)
-        }
-        else{
-          break
+// Get all subwords of the provided word from the dictionary
+app.get("/api/getwords", function(request, response) {
+  let word = request.query.word.trim().toUpperCase();
+  let new_subwords = [word];
+  let word_alpha = word.split("").sort();
+  // Loop over entire dictionary
+  for (let i = 0; i < wordList.length; i++) {
+    let word_temp = [...word_alpha];
+    let key = wordList[i].trim();
+    if (key.length > 2 && key.length <= word_alpha.length) {
+      let key_alpha = key.split("").sort();
+      let j = 0;
+      // Check all letters in the key against the letters in the current word
+      for (j = 0; j < key_alpha.length; j++) {
+        let ind = word_temp.indexOf(key_alpha[j]);
+        if (ind >= 0) {
+          word_temp.splice(ind, 1);
+        } else {
+          break;
         }
       }
-      if(j == key_alpha.length && key != word){  
-        new_subwords.push(key)
+      if (j == key_alpha.length && key != word) {
+        new_subwords.push(key);
       }
     }
   }
-  response.send(new_subwords)
-})
+  response.send(new_subwords);
+});
 
-app.get('/api/getdefinition', function(request,response) {
-  let word = request.query.word.trim().toUpperCase()
-  let def = wordDefs[word]
-  response.send(def)
-})
+// Get definintion of the provided word from the dictionary
+app.get("/api/getdefinition", function(request, response) {
+  let word = request.query.word.trim().toUpperCase();
+  let def = wordDefs[word];
+  response.send(def);
+});
 
-// Start a HTTPS server
+// Configure an HTTPS server
 const options = {
-    key: fs.readFileSync( key_path ),
-    cert: fs.readFileSync( cert_path ),
-    passphrase : 'texttwistkey'
-}
-server = require( 'https' ).createServer( options, app )
+  key: fs.readFileSync(key_path),
+  cert: fs.readFileSync(cert_path),
+  passphrase: "texttwistkey"
+};
+server = require("https").createServer(options, app);
 
-server.listen( port, function() {
-  console.log( 'Express server listening at: https://' + server.address().address + ":" + server.address().port )
-})
+server.listen(port, function() {
+  console.log(
+    "Express server listening at: https://" +
+      server.address().address +
+      ":" +
+      server.address().port
+  );
+});
 
+// Turn array into key-value pairs
 function objectify(array) {
   return array.reduce(function(p, c) {
-     p[c[0]] = c[1];
-     return p;
+    p[c[0]] = c[1];
+    return p;
   }, {});
 }
--- public_html/main.js
+++ public_html/main.js
@@ -1,273 +1,283 @@
-var currentWord = ""
-var answerWords = []
-var numEnteredWords = 0
-var lastEnteredWord = ""
+// Global variables to track state of the game
+var currentWord = "";
+var answerWords = [];
+var numEnteredWords = 0;
+var lastEnteredWord = "";
 
-jQuery( document ).ready( function( $ ) {
-  
-  $( '#twistBtn' ).click( function( event )
-  {
-    $('#letterInput').removeClass( 'is-valid' )
-    $('#letterInput').removeClass( 'is-invalid' )
-    let letterBtns = document.querySelector('#letterBank');
+jQuery(document).ready(function($) {
+  // Shuffle the order of the letters in the letter bank
+  $("#twistBtn").click(function(event) {
+    $("#letterInput").removeClass("is-valid");
+    $("#letterInput").removeClass("is-invalid");
+    let letterBtns = document.querySelector("#letterBank");
     for (let i = letterBtns.children.length; i >= 0; i--) {
-        letterBtns.appendChild(letterBtns.children[Math.random() * i | 0]);
+      letterBtns.appendChild(letterBtns.children[(Math.random() * i) | 0]);
     }
-  })
-  
-  $( '#lastBtn' ).click( function( event )
-  {
-    $('#letterInput').removeClass( 'is-valid' )
-    $('#letterInput').removeClass( 'is-invalid' )
-    $( '#enterBtn' ).prop( 'disabled', false )
-    $( '#letterInput' ).val( lastEnteredWord )
-    let letterBtns = document.querySelector('#letterBank');
-    for(let j = 0; j < lastEnteredWord.length; j++){
+  });
+
+  // Populate input field with last entered word, valid or not
+  $("#lastBtn").click(function(event) {
+    $("#letterInput").removeClass("is-valid");
+    $("#letterInput").removeClass("is-invalid");
+    $("#enterBtn").prop("disabled", false);
+    $("#letterInput").val(lastEnteredWord);
+    let letterBtns = document.querySelector("#letterBank");
+    for (let j = 0; j < lastEnteredWord.length; j++) {
       for (let i = 0; i < letterBtns.children.length; i++) {
-        if( letterBtns.children[i].innerHTML == lastEnteredWord.charAt(j).toUpperCase())
-        {
-          if(letterBtns.children[i].style.display != 'none')
-          {
-            letterBtns.children[i].style.display = 'none'
-            break 
+        if (
+          letterBtns.children[i].innerHTML ==
+          lastEnteredWord.charAt(j).toUpperCase()
+        ) {
+          if (letterBtns.children[i].style.display != "none") {
+            letterBtns.children[i].style.display = "none";
+            break;
           }
         }
-      } 
-    }
-  })
-  
-  $( '#clearBtn' ).click( function( event )
-  {
-    $('#letterInput').removeClass( 'is-valid' )
-    $('#letterInput').removeClass( 'is-invalid' )
-    $( '#enterBtn' ).prop( 'disabled', true )
-    $( '#letterInput' ).val( '' )
-    let letterBtns = document.querySelector('#letterBank')
-    for (let i = 0; i < letterBtns.children.length; i++) {
-      letterBtns.children[i].style.display = 'block'
-    }
-  })
-  
-  $( '#letterInput' ).keyup( function( event )
-  {
-    $('#letterInput').removeClass( 'is-valid' )
-    $('#letterInput').removeClass( 'is-invalid' )
-    if( event.originalEvent != undefined && event.originalEvent.code === "Enter")
-    {
-      if( ! $( '#enterBtn' ).is( ':disabled' ) )
-      {
-         $( '#enterBtn' ).click()
       }
     }
-    let letterBtns = document.querySelector('#letterBank');
-    let enteredWord = $('#letterInput').val()
+  });
+
+  // Erase input field
+  $("#clearBtn").click(function(event) {
+    $("#letterInput").removeClass("is-valid");
+    $("#letterInput").removeClass("is-invalid");
+    $("#enterBtn").prop("disabled", true);
+    $("#letterInput").val("");
+    let letterBtns = document.querySelector("#letterBank");
     for (let i = 0; i < letterBtns.children.length; i++) {
-      letterBtns.children[i].style.display = 'block'
+      letterBtns.children[i].style.display = "block";
     }
-    for(let j = 0; j < enteredWord.length; j++){
+  });
+
+  // Validate input field, and update letter bank accordingly
+  $("#letterInput").keyup(function(event) {
+    $("#letterInput").removeClass("is-valid");
+    $("#letterInput").removeClass("is-invalid");
+    if (
+      event.originalEvent != undefined &&
+      event.originalEvent.code === "Enter"
+    ) {
+      if (!$("#enterBtn").is(":disabled")) {
+        $("#enterBtn").click();
+      }
+    }
+    let letterBtns = document.querySelector("#letterBank");
+    let enteredWord = $("#letterInput").val();
+    for (let i = 0; i < letterBtns.children.length; i++) {
+      letterBtns.children[i].style.display = "block";
+    }
+    for (let j = 0; j < enteredWord.length; j++) {
       for (let i = 0; i < letterBtns.children.length; i++) {
-        if( letterBtns.children[i].innerHTML == enteredWord.charAt(j).toUpperCase())
-        {
-          if(letterBtns.children[i].style.display != 'none')
-          {
-            letterBtns.children[i].style.display = 'none'
-            break 
+        if (
+          letterBtns.children[i].innerHTML ==
+          enteredWord.charAt(j).toUpperCase()
+        ) {
+          if (letterBtns.children[i].style.display != "none") {
+            letterBtns.children[i].style.display = "none";
+            break;
           }
         }
-      } 
+      }
     }
-    if( event.target.checkValidity() )
-    {
-      $( '#enterBtn' ).prop( 'disabled', false )
+    if (event.target.checkValidity()) {
+      $("#enterBtn").prop("disabled", false);
+    } else {
+      $("#enterBtn").prop("disabled", true);
     }
-    else{
-      $( '#enterBtn' ).prop( 'disabled', true )
-    }
-  })
-  
-  $( '#enterBtn' ).click( function( event )
-  {  
-    $('#letterInput').removeClass( 'is-valid' )
-    $('#letterInput').removeClass( 'is-invalid' )
-    let test_word = $( '#letterInput' ).val().toUpperCase()
-    lastEnteredWord = test_word
-    $( '#letterInput' ).val( '' )
-    if(answerWords.includes(test_word)){
-      if($( '#table'+test_word ).html().indexOf('---') >= 0)
-      {
-        $( '#table'+test_word ).fadeOut( 150, function(){
-          $( '#table'+test_word ).html( test_word )
-          numEnteredWords++
-          $( '#table'+test_word ).fadeIn( 250, function() {
-            if(numEnteredWords == answerWords.length){
-              $( '#introMsg' ).fadeOut( 150, function(){
-                $( '#introMsg' ).html('You Win!!')
-                $( '#introMsg' ).fadeIn( 250 )
-              })
+  });
+
+  // Check if word is valid, and reveal it in the table
+  $("#enterBtn").click(function(event) {
+    $("#letterInput").removeClass("is-valid");
+    $("#letterInput").removeClass("is-invalid");
+    let test_word = $("#letterInput")
+      .val()
+      .toUpperCase();
+    lastEnteredWord = test_word;
+    $("#letterInput").val("");
+    if (answerWords.includes(test_word)) {
+      if (
+        $("#table" + test_word)
+          .html()
+          .indexOf("---") >= 0
+      ) {
+        $("#table" + test_word).fadeOut(150, function() {
+          $("#table" + test_word).html(test_word);
+          numEnteredWords++;
+          $("#table" + test_word).fadeIn(250, function() {
+            if (numEnteredWords == answerWords.length) {
+              $("#introMsg").fadeOut(150, function() {
+                $("#introMsg").html("You Win!!");
+                $("#introMsg").fadeIn(250);
+              });
             }
-          })
-        })  
+          });
+        });
       }
-      $('#letterInput').addClass( 'is-valid' )
+      $("#letterInput").addClass("is-valid");
+    } else {
+      $("#letterInput").addClass("is-invalid");
     }
-    else{
-      $('#letterInput').addClass( 'is-invalid' )
-    }
-    let letterBtns = document.querySelector('#letterBank');
+    let letterBtns = document.querySelector("#letterBank");
     for (let i = 0; i < letterBtns.children.length; i++) {
-      letterBtns.children[i].style.display = 'block'
+      letterBtns.children[i].style.display = "block";
     }
-  })
-  
-  $( '#newGameBtn' ).click( function( event )
-  {
-    $('#introMsg').html('Loading game...')
-    $( '#wordTable' ).fadeOut( 300, function(){
-      getWordAndSubs()
-    })
-  })
-  
+  });
+
+  // Clear current game and create a fresh one
+  $("#newGameBtn").click(function(event) {
+    $("#introMsg").html("Loading game...");
+    $("#wordTable").fadeOut(300, function() {
+      getWordAndSubs();
+    });
+  });
+
   //Start game
-  $( '#wordTable' ).fadeOut( 10, function(){
-    getWordAndSubs()
-  })
-})
+  $("#wordTable").fadeOut(10, function() {
+    getWordAndSubs();
+  });
+});
 
-function getWordAndSubs(){
-  $.get( '/api/randomword', function( data )
-  {
-    currentWord = data
-    $.get( '/api/getwords?word='+currentWord, function( data )
-    {
-      answerWords = data
-      if(answerWords.length > 50){
-        getWordAndSubs()
-      }
-      else{
+function getWordAndSubs() {
+  $.get("/api/randomword", function(data) {
+    currentWord = data;
+    $.get("/api/getwords?word=" + currentWord, function(data) {
+      answerWords = data;
+      // If game will take too long, find another
+      if (answerWords.length > 50) {
+        getWordAndSubs();
+      } else {
+        // Set up game front end
         answerWords.sort((a, b) => a.length - b.length || a.localeCompare(b));
-        $( '#letterBank' ).html('')
-        let word_arr = currentWord.split('').sort()
-        word_arr.forEach( function( w )
-        {
-          $( '#letterBank' ).append(`
-            <button type="button" class="btn btn-outline-primary flex-fill word-button">`+w+`</button>
-          `) 
-        })
-        populateWordTable()
-        $( '#wordTable' ).fadeIn( 500 )
-        $('#introMsg').html('Find all of the subwords!')
-        setLetterListener()
-        $( '#twistBtn' ).click()
-        
+        populateLetterBank();
+        populateWordTable();
+        $("#wordTable").fadeIn(500);
+        $("#introMsg").html("Find all of the subwords!");
+        $("#twistBtn").click();
       }
-    })
-  })
+    });
+  });
 }
-  
-function populateWordTable(){ 
-  $( '#letterNumHeaders' ).html('')
-  $( '#wordTableBody' ).html('')
-  let prevLen = 2
-  let ind = 1
-  answerWords.forEach( function( w ){
-    if(w.length > prevLen){
-      $( '#letterNumHeaders' ).append(`
-        <th scope="col">`+w.length+` Chars</th>
-      `)
-      prevLen = w.length
-      ind = 1
+
+function populateLetterBank() {
+  $("#letterBank").html("");
+  let word_arr = currentWord.split("").sort();
+  word_arr.forEach(function(w) {
+    $("#letterBank").append(
+      '<button type="button" class="btn btn-outline-primary flex-fill word-button">' +
+        w +
+        '</button>'
+    );
+  });
+  setLetterListener();
+}
+
+function setLetterListener() {
+  $("#letterBank button").click(function(event) {
+    $("#letterInput").val($("#letterInput").val() + event.target.innerText);
+    if (
+      $("#letterInput").val().length >= 3 &&
+      $("#letterInput").val().length <= 32
+    ) {
+      $("#enterBtn").prop("disabled", false);
+    } else {
+      $("#enterBtn").prop("disabled", true);
     }
-    if( $( '#wordTableBody tr:nth-child('+ind+')').length > 0 ){
-      let newWordElem = ''
-      let numCells = ($( '#wordTableBody tr:nth-child('+ind+')')[0]).cells.length
-      for(let i=numCells; i < w.length-3; i++){
-        if( $( '#letterNumHeaders th:nth-child('+(i+1)+')' )[0].innerHTML.indexOf(i+3) >= 0 ){
-          newWordElem += '<td></td>'
+  });
+}
+
+function populateWordTable() {
+  $("#letterNumHeaders").html("");
+  $("#wordTableBody").html("");
+  let prevLen = 2;
+  let ind = 1;
+  // Add all subwords to table
+  // Need to make sure all words are in the correct columns & rows
+  answerWords.forEach(function(w) {
+    if (w.length > prevLen) {
+      $("#letterNumHeaders").append(
+        '<th scope="col">' + w.length + " Chars</th>"
+      );
+      prevLen = w.length;
+      ind = 1;
+    }
+    if ($("#wordTableBody tr:nth-child(" + ind + ")").length > 0) {
+      let newWordElem = "";
+      let numCells = $("#wordTableBody tr:nth-child(" + ind + ")")[0].cells
+        .length;
+      for (let i = numCells; i < w.length - 3; i++) {
+        if (
+          $(
+            "#letterNumHeaders th:nth-child(" + (i + 1) + ")"
+          )[0].innerHTML.indexOf(i + 3) >= 0
+        ) {
+          newWordElem += "<td></td>";
         }
-        
       }
-      newWordElem += '<td><div class="tableWord" id="table'+w+'">'
-      for(let i=0; i < w.length; i++)
-      {
-        newWordElem += '-'
+      newWordElem += '<td><div class="tableWord" id="table' + w + '">';
+      for (let i = 0; i < w.length; i++) {
+        newWordElem += "-";
       }
-      newWordElem += '</div></td>'
-      $( '#wordTableBody tr:nth-child('+ind+')').append(newWordElem)
+      newWordElem += "</div></td>";
+      $("#wordTableBody tr:nth-child(" + ind + ")").append(newWordElem);
+    } else if (
+      $("#wordTableBody tr:nth-child(" + ind + ")").length == 0 &&
+      w.length > 3
+    ) {
+      let newWordElem = "<tr>";
+      for (let i = 0; i < w.length - 3; i++) {
+        newWordElem += "<td></td>";
+      }
+      newWordElem += '<td><div class="tableWord" id="table' + w + '">';
+      for (let i = 0; i < w.length; i++) {
+        newWordElem += "-";
+      }
+      newWordElem += "</div></td></tr>";
+      $("#wordTableBody").append(newWordElem);
+    } else {
+      let newWordElem = '<tr><td><div class="tableWord" id="table' + w + '">';
+      for (let i = 0; i < w.length; i++) {
+        newWordElem += "-";
+      }
+      newWordElem += "</div></td></tr>";
+      $("#wordTableBody").append(newWordElem);
     }
-    else if( $( '#wordTableBody tr:nth-child('+ind+')').length == 0 && w.length > 3 ){
-      let newWordElem = '<tr>'
-      for(let i=0; i < w.length-3; i++)
-      {
-        newWordElem += '<td></td>'
-      }
-      newWordElem += '<td><div class="tableWord" id="table'+w+'">'
-      for(let i=0; i < w.length; i++)
-      {
-        newWordElem += '-'
-      }
-      newWordElem += '</div></td></tr>'
-      $( '#wordTableBody' ).append(newWordElem)
-    }
-    else{
-      let newWordElem = '<tr><td><div class="tableWord" id="table'+w+'">'
-      for(let i=0; i < w.length; i++)
-      {
-        newWordElem += '-'
-      }
-      newWordElem += '</div></td></tr>'
-      $( '#wordTableBody' ).append(newWordElem) 
-    }
-    ind++
-  })
-  setTableWordListener()
+    ind++;
+  });
+  setTableWordListener();
 }
 
-function setLetterListener()
-{
-  $( '#letterBank button').click( function( event )
-  {
-    $( '#letterInput' ).val($( '#letterInput' ).val()+event.target.innerText)
-    if($( '#letterInput' ).val().length >= 3 && $( '#letterInput' ).val().length <= 32){
-      $( '#enterBtn' ).prop( 'disabled', false )
+function setTableWordListener() {
+  $("table .tableWord").click(function(event) {
+    if (event.currentTarget.innerHTML.indexOf("---") < 0) {
+      let clickedWord = event.currentTarget.id.substring(5);
+      $.get("/api/getdefinition?word=" + clickedWord, function(data) {
+        $("#definitionModalLabel").html(clickedWord);
+        $("#definitionModalBody").html(data);
+        $("#definitionModal").modal("toggle");
+      });
     }
-    else{
-      $( '#enterBtn' ).prop( 'disabled', true )
-    }
-  })
+  });
 }
 
-function setTableWordListener()
-{
-  $( 'table .tableWord' ).click( function( event )
-  {
-    if(event.currentTarget.innerHTML.indexOf('---') < 0)
-    {
-      let clickedWord = event.currentTarget.id.substring(5)
-      $.get( '/api/getdefinition?word='+clickedWord, function( data )
-      {
-        $( '#definitionModalLabel' ).html( clickedWord )
-        $( '#definitionModalBody' ).html( data )
-        $( '#definitionModal' ).modal( 'toggle' )
-      })
-    }    
-  })
-}
-
-function showAlert( message, alertClass )
-{
-  $( '#alert_placeholder' ).hide()
-  $( '#alert_placeholder' ).html( `<div class='alert alert-dismissable ` + alertClass + `' id='topAlert' role='alert'>
-      <span>` + message + `</span>
-      <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
-          <span aria-hidden='true'>&times</span>
+function showAlert(message, alertClass) {
+  $("#alert_placeholder").hide();
+  $("#alert_placeholder").html(
+    `<div class="alert alert-dismissable ` +
+      alertClass +
+      `" id="topAlert" role="alert">
+      <span>` +
+      message +
+      `</span>
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times</span>
       </button>
-  </div>` )
-  $( '#alert_placeholder' ).slideToggle( 400 )
-  window.setTimeout( function()
-  {
-    $( '#topAlert' ).slideToggle( 400, function()
-    {
-      $( this ).remove() 
-    })
-  }, 5000)
+    </div>`
+  );
+  $("#alert_placeholder").slideToggle(400);
+  window.setTimeout(function() {
+    $("#topAlert").slideToggle(400, function() {
+      $(this).remove();
+    });
+  }, 5000);
 }
